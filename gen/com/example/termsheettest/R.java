f33dd29a61c603c70770528523f5782e0b57ea65
mvaishnavi <vaishnavi4@gmail.com>
2015-11-02 23:35:13 +0530

    Modifications made.

--- gen/com/example/termsheettest/R.java
+++ gen/com/example/termsheettest/R.java
@@ -1134,9 +1134,9 @@ containing a value of this type.
         public static final int showHome=0x7f050004;
         public static final int showTitle=0x7f050006;
         public static final int split_action_bar=0x7f05001e;
+        public static final int statusBox=0x7f05003c;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
-        public static final int text_view_id=0x7f05003c;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
