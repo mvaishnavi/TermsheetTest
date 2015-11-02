0a97267b377c55971b199b891e9d4490e184496b
mvaishnavi <vaishnavi4@gmail.com>
2015-11-03 04:52:45 +0530

    Modifications made in activity-view relevant alignment and refactored http action to a seperate method.

--- src/com/example/termsheettest/MainActivity.java
+++ src/com/example/termsheettest/MainActivity.java
@@ -1,5 +1,4 @@
 package com.example.termsheettest;
-
 import java.io.IOException;
 
 import org.apache.http.HttpResponse;
@@ -8,58 +7,43 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		TextView text = (TextView) findViewById(R.id.statusBox);
+		text.setText("Loading Response..");
+		requestClient();
+	}
+
+	private void requestClient() {
 		HttpClient client = new DefaultHttpClient();
 		HttpGet request = new HttpGet("http://termsheet.io");
 		HttpResponse response = null;
-
 		TextView text = (TextView) findViewById(R.id.statusBox);
-
 		try {
 			response = client.execute(request);
 		} catch (ClientProtocolException e) {
 
 			text.setText("ClientProtocolException thrown");
 		} catch (IOException e) {
-			
+
 			text.setText("IOException thrown");
 		}
 		int statusCode = response.getStatusLine().getStatusCode();
 		if (statusCode >= 200 && statusCode < 300) {
 			text.setText("The request is OK. Site is Working.");
+			text.setTextColor(Color.parseColor("#008000"));
 		} else {
 			text.setText("The request is NOT Successful.");
+			text.setTextColor(Color.parseColor("#FF0000"));
 		}
-
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
 	}
 }
