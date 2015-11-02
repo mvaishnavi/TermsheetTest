d4936d5eee303fdcc730ca20a697b61897983a5e
mvaishnavi <vaishnavi4@gmail.com>
2015-11-02 22:45:32 +0530

    Termsheet Test App

---
+++ src/com/example/termsheettest/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.termsheettest;
+
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) 
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        HttpClient client = new DefaultHttpClient();
+    	HttpGet request = new HttpGet("http://www.example.com"); 
+    	HttpResponse response = null;
+        	
+    	
+    	TextView text= (TextView) findViewById(R.id.text_view_id);
+       
+        try {
+        	response = client.execute(request);
+        } catch (ClientProtocolException e) {
+			
+        	text.setText("exception thrown");
+		} catch (IOException e) {
+			
+			text.setText("exception thrown");
+		}
+        int statusCode = response.getStatusLine().getStatusCode();
+			if(statusCode>=200 && statusCode<300 )
+			{
+				text.setText("The request is OK. Site is Working.");
+			}
+			else
+			{
+				text.setText("The request is NOT Successful.");
+			}
+		
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
